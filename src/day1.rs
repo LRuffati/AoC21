5992b3067e77071692a917a2551c99a6e47b16cc
lruffati <lruffati@gmail.com>
2021-12-01 08:39:47 +0200

    Use reader instead of read_to_string and reformat

--- src/day1.rs
+++ src/day1.rs
@@ -1,36 +1,73 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 struct Acc {
     prev: Option<usize>,
-    count: usize
+    count: usize,
 }
 
-pub fn solve_a(input: String){
-    let mut nums: Vec<usize> = input.lines()
-                    .map(|num| num.parse::<usize>().unwrap())
-                    .collect();
-    let res: Acc = nums.iter().fold(Acc{prev: None, count: 0}, |acc: Acc, n| {
-        if acc.prev.is_none(){
-            Acc{prev: Some(n.clone()), count: acc.count}
-        } else {
-            Acc{prev: Some(n.clone()), count: if *n>acc.prev.unwrap() {acc.count+1} else {acc.count}}
-        }
-    });
+pub fn solve_a(input: BufReader<File>) {
+    let res: Acc = input
+        .lines()
+        .map(|num| num.unwrap().parse::<usize>().unwrap())
+        .fold(
+            Acc {
+                prev: None,
+                count: 0,
+            },
+            |acc: Acc, n| {
+                if acc.prev.is_none() {
+                    Acc {
+                        prev: Some(n.clone()),
+                        count: acc.count,
+                    }
+                } else {
+                    Acc {
+                        prev: Some(n.clone()),
+                        count: if n > acc.prev.unwrap() {
+                            acc.count + 1
+                        } else {
+                            acc.count
+                        },
+                    }
+                }
+            },
+        );
     println!("{}", res.count);
 }
 
-pub fn solve_b(input: String){
-    let nums: Vec<usize> = input.lines().map(|num| num.parse().unwrap()).collect();
+pub fn solve_b(input: BufReader<File>) {
+    let nums: Vec<usize> = input
+        .lines()
+        .map(|num| num.unwrap().parse().unwrap())
+        .collect();
     let first = &nums[0..];
     let sec = &nums[1..];
     let third = &nums[2..];
-    let res: Acc = first.iter().zip(sec.iter()).zip(third.iter())
-        .fold(Acc {prev: None, count: 0}, |acc: Acc, ((a,b), c)|{
-            let sum = a+b+c;
-            if acc.prev.is_none(){
-                Acc{prev: Some(sum), count: acc.count}
+    let res: Acc = first.iter().zip(sec.iter()).zip(third.iter()).fold(
+        Acc {
+            prev: None,
+            count: 0,
+        },
+        |acc: Acc, ((a, b), c)| {
+            let sum = a + b + c;
+            if acc.prev.is_none() {
+                Acc {
+                    prev: Some(sum),
+                    count: acc.count,
+                }
             } else {
-                Acc{prev: Some(sum), count: if sum>acc.prev.unwrap() {acc.count+1} else {acc.count}}
+                Acc {
+                    prev: Some(sum),
+                    count: if sum > acc.prev.unwrap() {
+                        acc.count + 1
+                    } else {
+                        acc.count
+                    },
+                }
             }
-        });
+        },
+    );
 
     println!("{}", res.count);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod day1;
 use std::fs;
-
+use std::fs::File;
+use std::io::BufReader;
 
 fn main() {
-    let mut input: String = fs::read_to_string("inputs/01a").unwrap();
-    day1::solve_b(input);
+    let file = File::open("inputs/01a").expect("Can't find file");
+    let reader = BufReader::new(file);
+    //let input: String = fs::read_to_string("inputs/01a").unwrap();
+    day1::solve_a(reader);
 }
