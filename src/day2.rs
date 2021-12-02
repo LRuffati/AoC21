9b279c83fc37ec6bcd39be6a7fd147236b2e3c22
lruffati <lruffati@gmail.com>
2021-12-02 09:00:46 +0200

    Second day with regex

---
+++ src/day2.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::ops::Add;
+use regex::Regex;
+use crate::day2::Command::{Down, Forward};
+
+
+struct Position {
+    depth: usize,
+    forward: usize,
+    aim: isize,
+}
+
+enum Command {
+    Forward(usize),
+    Up(usize),
+    Down(usize),
+}
+
+impl Command {
+    fn parse(input: String, re: &Regex) -> Self {
+        let matches = re.captures(input.as_str()).unwrap();
+        let number = matches.get(4).map(|num| {
+            let txt = num.as_str();
+            txt.parse::<usize>().unwrap()
+        }).unwrap();
+        if matches.get(1).is_some() {
+            Forward(number)
+        } else if matches.get(2).is_some() {
+            Down(number)
+        }else {
+            Command::Up(number)
+        }
+    }
+}
+
+impl Position {
+    fn add(self, rhs: Command) -> Self {
+        match rhs {
+            Forward(x) => Position {forward: self.forward+x, ..self},
+            Command::Up(x) => Position {depth: self.depth-x, ..self},
+            Down(x) => Position {depth: self.depth+x, ..self},
+        }
+    }
+
+    fn add2(self, rhs: Command) -> Self {
+        match rhs {
+            Forward(x) => {
+                let f = self.forward + x;
+                let d = (self.depth as isize) + self.aim * (x as isize);
+                Position{forward: f, depth: d as usize, ..self}
+            },
+            Command::Up(x) => Position{aim: self.aim-(x as isize), ..self},
+            Down(x) => Position{aim: self.aim+(x as isize), ..self},
+        }
+    }
+}
+
+pub fn solve_a(input: BufReader<File>) {
+    let re: Regex = Regex::new(r"(?:(forward)|(down)|(up)) ([0-9]+)").unwrap();
+    let final_p: Position = input.lines()
+        .map(|line| Command::parse(line.unwrap(), &re))
+        .fold(Position{depth: 0, forward: 0, aim: 0}, |p, c| p.add2(c));
+    println!("{}", final_p.forward*final_p.depth)
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
-mod day1;
+mod day2;
+
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
-    let file = File::open("inputs/01a").expect("Can't find file");
+    let file = File::open("inputs/02a").expect("Can't find file");
     let reader = BufReader::new(file);
     //let input: String = fs::read_to_string("inputs/01a").unwrap();
-    day1::solve_a(reader);
+    day2::solve_a(reader);
 }
